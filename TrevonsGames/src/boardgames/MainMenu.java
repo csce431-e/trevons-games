f500181898e70b6079d3b750be6292a99e59610a
tom <tvkwoodlands@gmail.com>
2013-03-07 14:39:56 -0600

    Solitaire init commit

--- TrevonsGames/src/boardgames/MainMenu.java
+++ TrevonsGames/src/boardgames/MainMenu.java
@@ -51,6 +51,7 @@ public class MainMenu extends javax.swing.JPanel {
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
         jButton8 = new javax.swing.JButton();
+        ip_input_box = new javax.swing.JTextField();
 
         jButton1.setText("Peg Solitaire");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -108,6 +109,13 @@ public class MainMenu extends javax.swing.JPanel {
             }
         });
 
+        ip_input_box.setText("Server IP here");
+        ip_input_box.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ip_input_boxActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -118,10 +126,13 @@ public class MainMenu extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE))
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(ip_input_box)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, 82, Short.MAX_VALUE)
+                            .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
@@ -144,7 +155,9 @@ public class MainMenu extends javax.swing.JPanel {
                         .addComponent(jButton7)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton8)))
-                .addGap(56, 56, 56)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(ip_input_box, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
                     .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -222,7 +235,22 @@ public class MainMenu extends javax.swing.JPanel {
         textLabel.setPreferredSize(new Dimension(300,100));
         gameFrame.getContentPane().add(textLabel,BorderLayout.CENTER);
         
-        client = new ClientPanel();
+        int a,b,c,d,index,oldindex;
+        String str = ip_input_box.getText();
+        oldindex = 0;
+        index = str.indexOf('.', 0);
+        a = Integer.parseInt(str.substring(oldindex, index)); //from 0 to index of first period
+        oldindex = index+1;
+        index = str.indexOf('.',index);
+        b = Integer.parseInt(str.substring(oldindex, index)); //from 0 to index of first period
+        oldindex = index+1;
+        index = str.indexOf('.',index);
+        c = Integer.parseInt(str.substring(oldindex, index)); //from 0 to index of first period
+        oldindex = index+1;
+        index = str.indexOf('.',index);
+        d = Integer.parseInt(str.substring(oldindex, index)); //from 0 to index of first period
+        int[] ip = new int[]{1,2,3};
+        client = new ClientPanel(ip);
         gameFrame.add((client));
         
         gameFrame.setLocationRelativeTo(null);
@@ -252,7 +280,12 @@ public class MainMenu extends javax.swing.JPanel {
         g.initGame();
     }//GEN-LAST:event_serverClicked
 
+    private void ip_input_boxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ip_input_boxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ip_input_boxActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField ip_input_box;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
