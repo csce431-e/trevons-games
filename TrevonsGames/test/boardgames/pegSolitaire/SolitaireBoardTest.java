334bd887e23763935790950414ac2c6251f6025e
tom <tvkwoodlands@gmail.com>
2013-04-01 16:57:24 -0500

    add test files for solitaire

--- TrevonsGames/test/boardgames/pegSolitaire/SolitaireBoardTest.java
+++ TrevonsGames/test/boardgames/pegSolitaire/SolitaireBoardTest.java
@@ -78,7 +78,7 @@ public class SolitaireBoardTest {
         SolitaireCoordinate result = instance.getCoordinate(x, y);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
     /**
