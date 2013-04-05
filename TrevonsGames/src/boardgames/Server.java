3cbc15ce52333dba476a9876bff76fbd96b3cd25
Thomas <tvkwoodlands@gmail.com>
2013-04-05 01:32:15 -0500

    Extensive clean-up to solitaire and improvements to server running (put in thread so you can close the window).  Also rearranged main menu to look slightly better.

--- TrevonsGames/src/boardgames/Server.java
+++ TrevonsGames/src/boardgames/Server.java
@@ -9,31 +9,25 @@ package boardgames;
  * @author Tom
  */
 
-//import boardgames.pegSolitaire.ClientPanel;
-//import boardgames.pegSolitaire.SolitaireMove;
-import boardgames.pegSolitaire.ClientPanel;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 class Slave implements Runnable
 {
-    Socket connection, conn2;
-    private String line, message;
+    Socket connection1, connection2;
+    private String message;
     
-    ObjectOutputStream out,out2;
-    ObjectInputStream in, in2;
+    ObjectOutputStream out1,out2;
+    ObjectInputStream in1, in2;
     
-    //Slave(Socket s, Socket s2, ObjectOutputStream o, ObjectInputStream i)
     Slave(Socket s, Socket s2, ObjectOutputStream o1, ObjectInputStream i1, ObjectOutputStream o2, ObjectInputStream i2)
     {
-        connection = s;
-        conn2 = s2;
-        out = o1;
-        in = i1;
+        connection1 = s;
+        connection2 = s2;
+        out1 = o1;
+        in1 = i1;
         out2 = o2;
         in2 = i2;
     }
@@ -44,8 +38,8 @@ class Slave implements Runnable
         {
             if(one)
             {
-                    out.writeObject(msg);
-                    out.flush();
+                    out1.writeObject(msg);
+                    out1.flush();
             }
             else
             {
@@ -66,23 +60,8 @@ class Slave implements Runnable
     {
         try
         {
-            /*
-            out = new ObjectOutputStream(connection.getOutputStream());
-            out.flush();
-            in = new ObjectInputStream(connection.getInputStream());
-            */
-            //out.reset();
-            //in.reset();
-            
-            /*out2 = new ObjectOutputStream(conn2.getOutputStream());
-            out2.flush();
-            in2 = new ObjectInputStream(conn2.getInputStream());
-*/
             sendMessage("Connection successful",true);
             sendMessage("Connection successful",false);
-            
-            //sendMessage("0",true); //going first
-            //sendMessage("Your not going first",false);
             
             while(true)
             {
@@ -91,7 +70,7 @@ class Slave implements Runnable
                     System.out.println("Waiting for move");
                     message = "nothing";
 
-                    message = (String)in.readObject();		//receiving from in1, so send to out2
+                    message = (String)in1.readObject();		//receiving from in1, so send to out2
                     System.out.println("Message received: "+message);
                     sendMessage(message, false);
                     System.out.println("Message sent: "+message);
@@ -107,25 +86,20 @@ class Slave implements Runnable
                 } 
                 catch (IOException ex) 
                 {
-                    Logger.getLogger(ClientPanel.class.getName()).log(Level.SEVERE, null, ex);
+                    ex.printStackTrace();
                 }   
             }
         }
-        /*catch (IOException ioe) 
-        {
-            System.out.println("IOException on socket listen: " + ioe);
-            ioe.printStackTrace();
-        }*/
         finally
         {
            try
            {
-                in.close();
-                out.close();
-                connection.close();
+                in1.close();
+                out1.close();
+                connection1.close();
                 in2.close();
                 out2.close();
-                conn2.close();
+                connection2.close();
            }
             catch(IOException ioException)
             {
@@ -152,78 +126,87 @@ public class Server
 	}
     }
     
-    //public static void main(String args[])
-    public void start()
+    class Server_Handler implements Runnable
     {
-        try
+        @Override
+        public void run()
         {
-            ArrayList<Pair> waiting_hosts = new ArrayList<>();
-            Socket connection;
-            int port_number = 2008; //back to 2006
-            Integer count = 1;
-            ServerSocket serverSocket = new ServerSocket(port_number);
-            
-            while(true)
+            try
             {
-                
-                System.out.println("Waiting for connection #"+ count.toString());
-                connection = serverSocket.accept();
-		System.out.println("Connection received from " + connection.getInetAddress().getHostName());
-		
-                ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
-                ObjectInputStream in = new ObjectInputStream(connection.getInputStream());
-                String message = (String)in.readObject();
-                //System.out.println(message);
-		boolean found = false;
-                System.out.println("Searching for host");
-		for(int i = 0; i<waiting_hosts.size(); i++)
-		{
-                    if(message.equals(waiting_hosts.get(i).game_name))
+                ArrayList<Pair> waiting_hosts = new ArrayList<>();
+                Socket connection;
+                int port_number = 2008; //back to 2006
+                Integer count = 1;
+                ServerSocket serverSocket = new ServerSocket(port_number);
+
+                while(true)
+                {
+
+                    System.out.println("Waiting for connection #"+ count.toString());
+                    connection = serverSocket.accept();
+                    System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+
+                    ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
+                    ObjectInputStream in = new ObjectInputStream(connection.getInputStream());
+                    String message = (String)in.readObject();
+                    //System.out.println(message);
+                    boolean found = false;
+                    System.out.println("Searching for host");
+                    for(int i = 0; i<waiting_hosts.size(); i++)
                     {
-                        System.out.println("found waiting host");
-                       found = true;
-                       out.writeObject("starting"); //tell second player starting
-                       out.flush();
-                       waiting_hosts.get(i).out_stream.writeObject("starting"); //tell original player starting
-                       waiting_hosts.get(i).out_stream.flush();
-                       //waiting_hosts.get(i).out_stream.close();
-                       //out.close();
-                       
-                       //Thread t = new Thread(new Slave(connection, waiting_hosts.get(i).socket,out,in));
-                       Thread t = new Thread(new Slave(connection, waiting_hosts.get(i).socket,waiting_hosts.get(i).out_stream,waiting_hosts.get(i).in_stream,out,in));
-                       waiting_hosts.remove(i);
-                       t.start();
+                        if(message.equals(waiting_hosts.get(i).game_name))
+                        {
+                            System.out.println("found waiting host");
+                           found = true;
+                           out.writeObject("starting"); //tell second player starting
+                           out.flush();
+                           waiting_hosts.get(i).out_stream.writeObject("starting"); //tell original player starting
+                           waiting_hosts.get(i).out_stream.flush();
+
+                           Thread t = new Thread(new Slave(connection, waiting_hosts.get(i).socket,waiting_hosts.get(i).out_stream,waiting_hosts.get(i).in_stream,out,in));
+                           waiting_hosts.remove(i);
+                           t.start();
+                        }
                     }
-		}
-		if(found == false)
-		{
-                    System.out.println("No host found, creating host");
-		   waiting_hosts.add(new Pair(connection, message, out, in));
-		   //send message from out to the client that says they are waiting for a connection
-                   out.writeObject("waiting");
-                   out.flush();
-		}
-                count++;
+                    if(found == false)
+                    {
+                        System.out.println("No host found, creating host");
+                       waiting_hosts.add(new Pair(connection, message, out, in));
+                       //send message from out to the client that says they are waiting for a connection
+                       out.writeObject("waiting");
+                       out.flush();
+                    }
+                    count++;
+                }
             }
-        }
-        catch(IOException ioException)
-        {
-            ioException.printStackTrace();
-        }
-        catch(ClassNotFoundException e)
-        {
-            System.out.println("class not found in server main");
-        }
-        finally
-        {
+            catch(IOException ioException)
+            {
+                ioException.printStackTrace();
+            }
+            catch(ClassNotFoundException e)
+            {
+                System.out.println("class not found in server main");
+            }
+            finally
+            {
+            }
         }
     }
     
+    //public static void main(String args[])
+    public void start()
+    {
+        Thread t = new Thread(new Server_Handler());
+        t.start();
+    }
 }
 
 /*
-try it without threads (just comment out the 3 thread lines at the end and see if it forms the connection w/ both
+exit button to kill game once it starts (even if/especially if it's not your turn)******most important, need to also send msg to other player that you quit
 
+* find new method for repainting correctly
+* stop it form painting the gui ON TOP OF the main menu for some reason
+* radio button "hover" take focus form the textbox which is pretty annoying
 
 
 
